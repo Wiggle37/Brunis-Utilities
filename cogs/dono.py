18e2f9896d6e330cd033e566ef0199c2546b66b6
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-01 16:54:27 -0400

    Update main.py

--- cogs/dono.py
+++ cogs/dono.py
@@ -21,14 +21,19 @@ class Dono(commands.Cog):
         result = cursor.fetchone()
 
         embed = discord.Embed(title='Donation Stats', description=None, color=0x00ff00)
-        embed.add_field(name='User:', value=f'{member.mention}({member.id})', inline=False)
-        embed.add_field(name='Donations:', value=f'`{result[0]}` donated in {ctx.guild.name}**')
+        embed.add_field(name='User:', value=f'{member.mention}(User id: {member.id})', inline=False)
+        embed.add_field(name='Giveaway Donations:', value=f'`{result[0]}` donated for giveaways in **{ctx.guild.name}**', inline=False)
+        embed.add_field(name='Heist Donations:', value=f'`Coming Soon...` donated for heists in **{ctx.guild.name}**', inline=False)
+        embed.add_field(name='Event Donations:', value=f'`Coming Soon...` donated for events in **{ctx.guild.name}**')
         await ctx.send(embed=embed)
 
         dbase.commit()
         dbase.close()
 
-    #Dono Add
+    '''
+    GIVEAWAY DONATIONS
+    '''
+    #Giveaway Dono Add
     @commands.command(aliases=['da'])
     @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
     async def dono_add(self, ctx, member: discord.Member, amount: int):
@@ -39,7 +44,7 @@ class Dono(commands.Cog):
         user = int(f'{member.id}')
         amount = int(f'{amount}')
 
-        cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) (user_id) DO UPDATE SET amount = amount + ?;", [guild, user, amount, amount])
+        cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT DO UPDATE SET amount = amount + ?;", [guild, user, amount, amount])
 
         await ctx.send(f"Donation note added for **{member}**\nThe amount added was **{amount}**")
 
@@ -82,6 +87,14 @@ class Dono(commands.Cog):
 
         dbase.commit()
         dbase.close()
+
+        '''
+        HEIST DONATIONS
+        '''
+
+        '''
+        EVENT DONATIONS
+        '''
 
 def setup(client):
     client.add_cog(Dono(client))
