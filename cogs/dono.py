9d4daeb47a59ac025322152596b9de20559bc028
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-01 19:01:28 -0400

    Update help.py and dono.py

--- cogs/dono.py
+++ cogs/dono.py
@@ -12,17 +12,32 @@ class Dono(commands.Cog):
         num = 10000000
         await ctx.send(f"{num:,}")
 
-    #Dono Check
+    '''
+    DONATIONS CHECK
+    '''
     @commands.command(aliases=['d'])
     async def dono(self, ctx, member: discord.Member):
         dbase = sqlite3.connect('bruni.db')
         cursor = dbase.cursor()
+        #Get Gaw Amount
         cursor.execute(f"SELECT amount FROM gaw_dono_logs WHERE guild_id = '{ctx.guild.id}' AND user_id = '{member.id}'")
-        result = cursor.fetchone()
+        result1 = cursor.fetchone()
+
+        
+        num = f'{result1[0]}'
+        print(f'{num:,}')
+
+        #Get Heist Amount
+        cursor.execute(f"SELECT amount FROM gaw_dono_logs WHERE guild_id = '{ctx.guild.id}' AND user_id = '{member.id}'")
+        result2 = cursor.fetchone()
+
+        #Get Event Amount
+        cursor.execute(f"SELECT amount FROM gaw_dono_logs WHERE guild_id = '{ctx.guild.id}' AND user_id = '{member.id}'")
+        result3 = cursor.fetchone()
 
         embed = discord.Embed(title='Donation Stats', description=None, color=0x00ff00)
         embed.add_field(name='User:', value=f'{member.mention}(User id: {member.id})', inline=False)
-        embed.add_field(name='Giveaway Donations:', value=f'`{result[0]}` donated for giveaways in **{ctx.guild.name}**', inline=False)
+        embed.add_field(name='Giveaway Donations:', value=f'`{result1[0]}` donated for giveaways in **{ctx.guild.name}**', inline=False)
         embed.add_field(name='Heist Donations:', value=f'`Coming Soon...` donated for heists in **{ctx.guild.name}**', inline=False)
         embed.add_field(name='Event Donations:', value=f'`Coming Soon...` donated for events in **{ctx.guild.name}**')
         await ctx.send(embed=embed)
@@ -34,9 +49,9 @@ class Dono(commands.Cog):
     GIVEAWAY DONATIONS
     '''
     #Giveaway Dono Add
-    @commands.command(aliases=['da'])
+    @commands.command(aliases=['gda'])
     @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
-    async def dono_add(self, ctx, member: discord.Member, amount: int):
+    async def gaw_dono_add(self, ctx, member: discord.Member, amount: int):
         dbase = sqlite3.connect('bruni.db')
         cursor = dbase.cursor()
 
@@ -44,7 +59,7 @@ class Dono(commands.Cog):
         user = int(f'{member.id}')
         amount = int(f'{amount}')
 
-        cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT DO UPDATE SET amount = amount + ?;", [guild, user, amount, amount])
+        cursor.execute("INSERT INTO dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT(user_id) DO UPDATE SET amount = amount + ?;", [guild, user, amount, amount])
 
         await ctx.send(f"Donation note added for **{member}**\nThe amount added was **{amount}**")
 
@@ -52,9 +67,9 @@ class Dono(commands.Cog):
         dbase.close()
 
     #Dono Remove
-    @commands.command(aliases=['dr'])
+    @commands.command(aliases=['gdr'])
     @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
-    async def dono_remove(self, ctx, member: discord.Member, amount: int):
+    async def gaw_dono_remove(self, ctx, member: discord.Member, amount: int):
         dbase = sqlite3.connect('bruni.db')
         cursor = dbase.cursor()
 
@@ -70,9 +85,9 @@ class Dono(commands.Cog):
         dbase.close()
 
     #Dono Reset
-    @commands.command(aliases=['drs'])
+    @commands.command(aliases=['gdrs'])
     @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
-    async def dono_reset(self, ctx, member: discord.Member):
+    async def gaw_dono_reset(self, ctx, member: discord.Member):
         dbase = sqlite3.connect('bruni.db')
         cursor = dbase.cursor()
 
@@ -88,13 +103,121 @@ class Dono(commands.Cog):
         dbase.commit()
         dbase.close()
 
-        '''
-        HEIST DONATIONS
-        '''
+    '''
+    HEIST DONATIONS
+    '''
+    #Giveaway Dono Add
+    @commands.command(aliases=['da'])
+    @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
+    async def heist_dono_add(self, ctx, member: discord.Member, amount: int):
+        dbase = sqlite3.connect('bruni.db')
+        cursor = dbase.cursor()
 
-        '''
-        EVENT DONATIONS
-        '''
+        guild = int(ctx.guild.id)
+        user = int(f'{member.id}')
+        amount = int(f'{amount}')
+
+        cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) (user_id) DO UPDATE SET amount = amount + ?;", [guild, user, amount, amount])
+
+        await ctx.send(f"Donation note added for **{member}**\nThe amount added was **{amount}**")
+
+        dbase.commit()
+        dbase.close()
+
+    #Dono Remove
+    @commands.command(aliases=['hdr'])
+    @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
+    async def heist_dono_remove(self, ctx, member: discord.Member, amount: int):
+        dbase = sqlite3.connect('bruni.db')
+        cursor = dbase.cursor()
+
+        guild = int(ctx.guild.id)
+        user = int(f'{member.id}')
+        amount = int(f'{amount}')
+
+        cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT(user_id) DO UPDATE SET amount = amount - ?;", [guild, user, amount, amount])
+
+        await ctx.send(f"Donation note removed for **{member}**\nThe amount removed was **{amount}**")
+
+        dbase.commit()
+        dbase.close()
+
+    #Dono Reset
+    @commands.command(aliases=['hdrs'])
+    @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
+    async def heist_dono_reset(self, ctx, member: discord.Member):
+        dbase = sqlite3.connect('bruni.db')
+        cursor = dbase.cursor()
+
+        guild = int(ctx.guild.id)
+        user = int(f'{member.id}')
+
+        amount = 0
+
+        cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT(user_id) DO UPDATE SET amount = amount = ?;", [guild, user, amount, amount])
+
+        await ctx.send(f"Donation note reset for **{member}**\nThe amount was set to **{amount}**")
+
+        dbase.commit()
+        dbase.close()
+
+    '''
+    EVENT DONATIONS
+    '''
+    #Giveaway Dono Add
+    @commands.command(aliases=['eda'])
+    @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
+    async def event_dono_add(self, ctx, member: discord.Member, amount: int):
+        dbase = sqlite3.connect('bruni.db')
+        cursor = dbase.cursor()
+
+        guild = int(ctx.guild.id)
+        user = int(f'{member.id}')
+        amount = int(f'{amount}')
+
+        cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) (user_id) DO UPDATE SET amount = amount + ?;", [guild, user, amount, amount])
+
+        await ctx.send(f"Donation note added for **{member}**\nThe amount added was **{amount}**")
+
+        dbase.commit()
+        dbase.close()
+
+    #Dono Remove
+    @commands.command(aliases=['edr'])
+    @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
+    async def event_dono_remove(self, ctx, member: discord.Member, amount: int):
+        dbase = sqlite3.connect('bruni.db')
+        cursor = dbase.cursor()
+
+        guild = int(ctx.guild.id)
+        user = int(f'{member.id}')
+        amount = int(f'{amount}')
+
+        cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT(user_id) DO UPDATE SET amount = amount - ?;", [guild, user, amount, amount])
+
+        await ctx.send(f"Donation note removed for **{member}**\nThe amount removed was **{amount}**")
+
+        dbase.commit()
+        dbase.close()
+
+    #Dono Reset
+    @commands.command(aliases=['edrs'])
+    @commands.has_any_role(785198646731604008, 785631914010214410, 784527745539375164, 810233857768554506) 
+    async def event_dono_reset(self, ctx, member: discord.Member):
+        dbase = sqlite3.connect('bruni.db')
+        cursor = dbase.cursor()
+
+        guild = int(ctx.guild.id)
+        user = int(f'{member.id}')
+
+        amount = 0
+
+        cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT(user_id) DO UPDATE SET amount = amount = ?;", [guild, user, amount, amount])
+
+        await ctx.send(f"Donation note reset for **{member}**\nThe amount was set to **{amount}**")
+
+        dbase.commit()
+        dbase.close()
 
 def setup(client):
     client.add_cog(Dono(client))
--- cogs/help.py
+++ cogs/help.py
@@ -11,21 +11,38 @@ class Help(commands.Cog):
     async def help(self, ctx, cog=None):
         if cog is None:
             help_embed = discord.Embed(title='Brunis Utilities', description='[Dank Merchants](https://discord.gg/S5sNmzfF9M)', color=0x00ff00)
-            help_embed.add_field(name='Donations', value='Track how much someone has donated in the server', inline=False)
-            help_embed.add_field(name='Coming Soon...', value='Coming Soon...', inline=False)
+            help_embed.add_field(name='Giveaway Donation', value='Track how much someone has donated for giveaways', inline=False)
+            help_embed.add_field(name='Heist Donations', value='Track how much someone has donated for heists', inline=False)
+            help_embed.add_field(name='Event Donations', value='Track how much someone has donated for events', inline=False)
             await ctx.send(embed=help_embed)
 
-        if cog == 'dono' or cog == 'donation':
+        if cog == 'giveaway' or cog == 'gaw':
             help_embed = discord.Embed(title='Donations', description='[Dank Merchants](https://discord.gg/S5sNmzfF9M)', color=0x00ff00)
-            help_embed.add_field(name='Dono', value='See how much someone has doneted in the server```b!dono <@member / id>```', inline=False)
-            help_embed.add_field(name='Dono_add', value='Add to someones donations(must have the giveaway manager or heist manger role)```b!da <@member / id> <amount(int)>```', inline=False)
-            help_embed.add_field(name='Dono_remove', value='Removes from someones donation amount```b!dr <@member / id> <amount(int)>```', inline=False)
-            help_embed.add_field(name='Dono_reset', value='Restes the donation amount for the specified number```b!drs <@member /id>```', inline=False)
+            help_embed.add_field(name='Dono', value='See how much someone has doneted in the server```b!d <@member / id>```', inline=False)
+            help_embed.add_field(name='Giveaway Dono Add', value='Add to someones donations(must have the giveaway manager or heist manger role)```b!gda <@member / id> <amount(int)>```', inline=False)
+            help_embed.add_field(name='Giveaway Dono Remove', value='Removes from someones donation amount```b!gdr <@member / id> <amount(int)>```', inline=False)
+            help_embed.add_field(name='Giveaway Dono Reset', value='Restes the donation amount for the specified number```b!gdrs <@member /id>```', inline=False)
+            await ctx.send(embed=help_embed)
+
+        if cog == 'heist':
+            help_embed = discord.Embed(title='Donations', description='[Dank Merchants](https://discord.gg/S5sNmzfF9M)', color=0x00ff00)
+            help_embed.add_field(name='Dono', value='See how much someone has doneted in the server```b!d <@member / id>```', inline=False)
+            help_embed.add_field(name='Heist Dono Add', value='Add to someones donations(must have the giveaway manager or heist manger role)```b!hda <@member / id> <amount(int)>```', inline=False)
+            help_embed.add_field(name='Heist Dono Remove', value='Removes from someones donation amount```b!hdr <@member / id> <amount(int)>```', inline=False)
+            help_embed.add_field(name='Heist Dono Reset', value='Restes the donation amount for the specified number```b!hdrs <@member /id>```', inline=False)
+            await ctx.send(embed=help_embed)
+        
+        if cog == 'event':
+            help_embed = discord.Embed(title='Donations', description='[Dank Merchants](https://discord.gg/S5sNmzfF9M)', color=0x00ff00)
+            help_embed.add_field(name='Dono', value='See how much someone has doneted in the server```b!d <@member / id>```', inline=False)
+            help_embed.add_field(name='Event Dono Add', value='Add to someones donations(must have the giveaway manager or heist manger role)```b!eda <@member / id> <amount(int)>```', inline=False)
+            help_embed.add_field(name='Event Dono Remove', value='Removes from someones donation amount```b!edr <@member / id> <amount(int)>```', inline=False)
+            help_embed.add_field(name='Event Dono Reset', value='Restes the donation amount for the specified number```b!edrs <@member /id>```', inline=False)
             await ctx.send(embed=help_embed)
 
         if cog == 'settings' or cog == 'config':
             help_embed = discord.Embed(title='Donations', description='[Dank Merchants](https://discord.gg/S5sNmzfF9M)', color=0x00ff00)
-            
+            help_embed.add_field(name='Coming Soon!...', value=None)
             await ctx.send(embed=help_embed)
 
 def setup(client):
