b6331f8bfbe11710a1ff0a4e038addfb7b3ee988
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-02 14:45:12 -0400

    Update owner.py, dono.py

--- cogs/dono.py
+++ cogs/dono.py
@@ -7,6 +7,34 @@ class Dono(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+
+    '''
+    ADD TO DATABASE
+    '''
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        dbase = sqlite3.connect('bruni.db')
+        cursor = dbase.cursor()
+
+        user = f'{ctx.author.id}'
+        guild = f'{ctx.guild.id}'
+
+        cursor.execute(f"SELECT user_id FROM gaw_dono_logs WHERE guild_id = '{ctx.guild.id}' AND user_id = '{ctx.author.id}'")
+        result = cursor.fetchone()
+
+        if result is None:
+            amount = 0
+            cursor.execute("INSERT INTO gaw_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT(user_id) DO UPDATE SET amount = amount + ?;", [guild, user, amount, amount])
+            cursor.execute("INSERT INTO heist_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT(user_id) DO UPDATE SET amount = amount + ?;", [guild, user, amount, amount])
+            cursor.execute("INSERT INTO event_dono_logs (guild_id, user_id, amount) VALUES (?, ?, ?) ON CONFLICT(user_id) DO UPDATE SET amount = amount + ?;", [guild, user, amount, amount])
+            print('added to db')
+
+        else:
+            pass
+
+        dbase.commit()
+        dbase.close()
+
     '''
     DONATIONS CHECK
     '''
@@ -14,29 +42,46 @@ class Dono(commands.Cog):
     async def dono(self, ctx, member: discord.Member):
         dbase = sqlite3.connect('bruni.db')
         cursor = dbase.cursor()
+
+        if member is None
+
         #Get Gaw Amount
         cursor.execute(f"SELECT amount FROM gaw_dono_logs WHERE guild_id = '{ctx.guild.id}' AND user_id = '{member.id}'")
         result1 = cursor.fetchone()
         if result1 is None:
-            await ctx.send('You havent donated anything for giveaways!')
+            await ctx.send('Donate to giveaways')
 
         #Get Heist Amount
         cursor.execute(f"SELECT amount FROM heist_dono_logs WHERE guild_id = '{ctx.guild.id}' AND user_id = '{member.id}'")
         result2 = cursor.fetchone()
         if result2 is None:
-            await ctx.send('You havent donated anything for heists!')
+            await ctx.send("Donate to heists")
 
         #Get Event Amount
         cursor.execute(f"SELECT amount FROM event_dono_logs WHERE guild_id = '{ctx.guild.id}' AND user_id = '{member.id}'")
         result3 = cursor.fetchone()
         if result3 is None:
-            await ctx.send('You havent donated anything for events!')
+            await ctx.send('Donate to events')
+
+        result1 = (result1[0])
+        new_result1 = ('{:,}'.format(result1))
+
+        result2 = (result2[0])
+        new_result2 = ('{:,}'.format(result2))
+
+        result3 = (result3[0])
+        new_result3 = ('{:,}'.format(result3))
+
+
+        all = result1 + result2 + result3
+        new_all = ('{:,}'.format(all))
 
         embed = discord.Embed(title='Donation Stats', description=None, color=0x00ff00)
         embed.add_field(name='User:', value=f'{member.mention}(User id: {member.id})', inline=False)
-        embed.add_field(name='Giveaway Donations:', value=f'`{result1[0]}` donated for giveaways in **{ctx.guild.name}**', inline=False)
-        embed.add_field(name='Heist Donations:', value=f'`{result2[0]}` donated for heists in **{ctx.guild.name}**', inline=False)
-        embed.add_field(name='Event Donations:', value=f'`{result3[0]}` donated for events in **{ctx.guild.name}**')
+        embed.add_field(name='Giveaway Donations:', value=f'`{new_result1}` donated for giveaways in **{ctx.guild.name}**', inline=False)
+        embed.add_field(name='Heist Donations:', value=f'`{new_result2}` donated for heists in **{ctx.guild.name}**', inline=False)
+        embed.add_field(name='Event Donations:', value=f'`{new_result3}` donated for events in **{ctx.guild.name}**', inline=False)
+        embed.add_field(name='Total Donations:', value=f'`{new_all}` donated in total for **{ctx.guild.name}**', inline=False)
         await ctx.send(embed=embed)
 
         dbase.commit()
--- cogs/owner.py
+++ cogs/owner.py
@@ -17,8 +17,34 @@ class Owner(commands.Cog):
 
     #Update
     @commands.command()
-    async def update(self, ctx):
-        pass
+    async def update1(self, ctx):
+        client = self.client
+        await ctx.message.delete()
+        await client.get_channel(827293945003376650).send(f'Ok, I am very proud to announce the newest bot in our server! **{client.user}**!\nSo for this bot is still so much in developement that it may have a some errors(probally a lot...)so if you ever see one PLEASE send a DM to <@765322777329664089> explaining the issue as much as you can so I can so my best to fix it. And also FYI me(<@765322777329664089>) and bruni(<@784172569153503332>) have full access to all of these command to bypass\n\nSoon I would like to add a vouching system which is the same as the other vouch bot but I make it(this will not bec coming this month because I other things to work on with this bot that are more important)! So now let talk commands who can access what commands and what they do\n\n__**Command Access:**__\n- `b!dono <member / id>` -> anyone can you this command \nAt the moment the only way to check your own donation is by doing `b!dono <@your user / id>` I know this is kind of incovenient but it is what it is and will try to be fixed soon')
+
+    @commands.command()
+    async def update2(self, ctx):
+        client = self.client
+        await ctx.message.delete()
+        await client.get_channel(827293945003376650).send('__**Giveaway Type Donations:**__\n- `b!gda <@member / id> <amount>` -> <@&785198646731604008>\nThis command allows giveaway managers add to the amount that someone has donated for giveaways\n\n- `b!gdr <@member / id> <amount>` -> <@&785198646731604008>\nThis command allows giveaway managers to remove from someones donated amount\n\n- `gdrs <@member / id>` -><@&785198646731604008>\nThis reset the specified members giveaways donation amount')
+
+    @commands.command()
+    async def update3(self, ctx):
+        client = self.client
+        await ctx.message.delete()
+        await client.get_channel(827293945003376650).send('__**Heist Type Donations:**__\n- `b!hda <@member / id> <amount>` -> <@&785631914010214410>\nThis command allows heist managers add to the amount that someone has donated for giveaways\n\n- `b!hdr <@member / id> <amount>` -> <@&785631914010214410>\nThis command allows heist managers to remove from someones donated amount\n\n- `hdrs <@member / id>` -> <@&785631914010214410>\nThis reset the specified members giveaways donation amount')
+
+    @commands.command()
+    async def update4(self, ctx):
+        client = self.client
+        await ctx.message.delete()
+        await client.get_channel(827293945003376650).send('__**Event Type Donations:**__\n- `b!eda <@member / id> <amount>` -> <@&791516116710064159>\nThis command allows event managers add to the amount that someone has donated for giveaways\n\n- `b!edr <@member / id> <amount>` -> <@&791516116710064159>\nThis command allows event managers to remove from someones donated amount\n\n- `edrs <@member / id>` -> <@&791516116710064159>\nThis reset the specified members giveaways donation amount')
+
+    @commands.command()
+    async def update5(self, ctx):
+        client = self.client
+        await ctx.message.delete()
+        await client.get_channel(827293945003376650).send('If you have any questions to do with anything with this bot feel free to DM <@765322777329664089>\nMore about the bot will be out later sometime!')
 
 def setup(client):
     client.add_cog(Owner(client))
