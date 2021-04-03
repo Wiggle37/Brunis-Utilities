0ba0c65b9bbeb19fee3d16d1c198cb38f2ee9d70
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-03 09:39:27 -0400

    Update main.py, dono.py

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import discord
 from discord import Activity, ActivityType, Color, Embed, User
 from discord.ext import commands
 from discord.ext.commands import Bot
-
 import os
 from dotenv import load_dotenv
-
 import sqlite3
-
 ###Intents###
 intents = discord.Intents.default()
 intents.members = True
@@ -23,43 +20,42 @@ client.remove_command('help')
 ###On Ready###
 @client.event
 async def on_ready():
-    print('Bot Online!')
+    print(f'{client.user} is online!')
 
     dbase = sqlite3.connect('bruni.db')
     cursor = dbase.cursor()
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS "gaw_dono_logs" (
-	"guild_id"	INTEGER,
-	"user_id"	INTEGER UNIQUE,
-	"amount"	INTEGER DEFAULT 0
-    )""")
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS "heist_dono_logs" (
-	"guild_id"	INTEGER,
-	"user_id"	INTEGER UNIQUE,
-	"amount"	INTEGER DEFAULT 0
-    )""")
-
-    cursor.execute("""CREATE TABLE IF NOT EXISTS "event_dono_logs" (
-	"guild_id"	INTEGER,
-	"user_id"	INTEGER UNIQUE,
-	"amount"	INTEGER DEFAULT 0
-    )""")
-
     cursor.execute("""CREATE TABLE IF NOT EXISTS "money_dono_logs" (
-	"guild_id"	INTEGER,
-	"user_id"	INTEGER UNIQUE,
-	"amount"	INTEGER DEFAULT 0
-    )""")
-
-    dbase.commit()
-    dbase.close()
+        "guild_id"	INTEGER,
+        "user_id"	INTEGER UNIQUE,
+        "amount"	INTEGER DEFAULT 0
+        )""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS "gaw_dono_logs" (
+        "guild_id"	INTEGER,
+        "user_id"	INTEGER UNIQUE,
+        "amount"	INTEGER DEFAULT 0
+        )""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS "heist_dono_logs" (
+        "guild_id"	INTEGER,
+        "user_id"	INTEGER UNIQUE,
+        "amount"	INTEGER DEFAULT 0
+        )""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS "event_dono_logs" (
+        "guild_id"	INTEGER,
+        "user_id"	INTEGER UNIQUE,
+        "amount"	INTEGER DEFAULT 0
+        )""")
+    cursor.execute("""CREATE TABLE "2.5k_event_dono_logs" (
+        "guild_id"	INTEGER,
+        "user_id"	INTEGER UNIQUE,
+        "amount"	INTEGER DEFAULT 0
+        )""")
     
+    dbase.commit()
+    dbase.close() 
 ###Cog Loader###
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
-
 ###Run Bot###
 load_dotenv()
 Bot_Token = os.getenv('Discord_Bot_Token')
