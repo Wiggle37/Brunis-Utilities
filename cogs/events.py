ce188ddce51a8a75dbafe1ea4691114faddac45e
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-03 11:52:15 -0400

    Update events.py

---
+++ cogs/events.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+class Events(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        client = self.client
+
+        join_embed = discord.Embed(title=f'Welcome To __**Dank Merchants**!__', description=f'{member.mention} has joined the server!', color=0x00ff00)
+        join_embed.add_field(name='What To Do', value=f'Make sure to go check out <#787343840108478474> for some info about how to get certain thing in the server and <#784547669619507201> for some self roles!')
+        join_embed.add_field(name=f'__**User Info:**__', value=f'Date created: {member.created_at}\nUser ID: {member.id}', inline=False)
+        await client.get_channel(784491141022220312).send(embed=join_embed)
+        await ctx.send(ctx.author.created_at)
+
+
+def setup(client):
+    client.add_cog(Events(client))
