e7d45c0506576a0e08e82bd3280c5c94b2a71148
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-05 14:57:46 -0400

    Update events.py

--- cogs/events.py
+++ cogs/events.py
@@ -1,12 +1,14 @@
 import discord
 from discord.ext import commands
 
+import sqlite3
+
 class Events(commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
-    #on Member Join
+    #On Member Join(Message)
     @commands.Cog.listener()
     async def on_member_join(self, member):
         client = self.client
@@ -16,6 +18,25 @@ class Events(commands.Cog):
         join_embed.add_field(name='What To Do', value=f'Make sure to go check out <#787343840108478474> for some info about how to get certain thing in the server and <#784547669619507201> for some self roles!')
         join_embed.add_field(name=f'__**User Info:**__', value=f'Date created: {member.created_at}\nUser ID: {member.id}', inline=False)
         await client.get_channel(784491141022220312).send(embed=join_embed)
+
+    #On Member Remove(Data Base)
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        dbase = sqlite3.connect('bruni.db')
+        cursor = dbase.cursor()
+
+        user_id = member.id
+
+        cursor.execute("DELETE FROM gaw_dono_logs WHERE user_id = ?", [user_id])
+        cursor.execute("DELETE FROM heist_dono_logs WHERE user_id = ?", [user_id])
+        cursor.execute("DELETE FROM event_dono_logs WHERE user_id = ?", [user_id])
+        cursor.execute("DELETE FROM special_event_dono_logs WHERE user_id = ?", [user_id])
+        cursor.execute("DELETE FROM money_dono_logs WHERE user_id = ?", [user_id])
+
+        print('User removed from db')
+
+        dbase.commit()
+        dbase.close()
 
 def setup(client):
     client.add_cog(Events(client))
