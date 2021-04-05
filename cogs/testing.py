c986dfe039ecfe8c39d551830e907e4183834f22
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-05 14:01:15 -0400

    Add testing.py

---
+++ cogs/testing.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+class Testing(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def join(self, ctx):
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    @commands.command()
+    async def leave(self, ctx):
+        await ctx.voice_client.disconnect()
+
+def setup(client):
+    client.add_cog(Testing(client))
