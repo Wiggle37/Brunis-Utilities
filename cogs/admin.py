a2abb6709308b47f15c469671806145be5ff67b5
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-06 11:52:57 -0400

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -6,5 +6,11 @@ class Admin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command()
+    @commands.has_any_role(784492058756251669, 784527745539375164, 785202756641619999, 788738308879941633) #Admin, Mod, Bruni, Bot Dev
+    async def addrole(self, ctx, member: discord.Member=None, role:discord.Role=None):
+        client = self.client
+        await member.add_roles(role)
+
 def setup(client):
     client.add_cog(Admin(client))
