31ff819be7b3628c9ab9f1cf171bd866cd48b2c6
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-06 19:07:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import discord
 from discord import Activity, ActivityType, Color, Embed, User
 from discord.ext import commands
 from discord.ext.commands import Bot
+
 import os
 from dotenv import load_dotenv
+
 import sqlite3
+
 ###Intents###
 intents = discord.Intents.default()
 intents.members = True
@@ -22,10 +25,12 @@ async def on_ready():
     print(f'{client.user} is online!')
 
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'over Dank Merchants'))
+
 ###Cog Loader###
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
+        
 ###Run Bot###
 load_dotenv()
 Bot_Token = os.getenv('Discord_Bot_Token')
