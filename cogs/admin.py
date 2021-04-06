b924f431e6ca2653c74982af517a693625625910
Y33T-Wiggle <misterernie37@gmail.com>
2021-04-06 16:45:21 -0400

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -6,11 +6,21 @@ class Admin(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    #Add Role
+    @commands.command(aliases=['ar'])
     @commands.has_any_role(784492058756251669, 784527745539375164, 785202756641619999, 788738308879941633) #Admin, Mod, Bruni, Bot Dev
     async def addrole(self, ctx, member: discord.Member=None, role:discord.Role=None):
         client = self.client
         await member.add_roles(role)
+        await ctx.send(f'Role added to **{member}**')
+
+    #Remove Role
+    @commands.command(aliases=['rr'])
+    @commands.has_any_role(784492058756251669, 784527745539375164, 785202756641619999, 788738308879941633) #Admin, Mod, Bruni, Bot Dev
+    async def removerole(self, ctx, member: discord.Member=None, role:discord.Role=None):
+        client = self.client
+        await member.remove_roles(role)
+        await ctx.send(f'Role removed from **{member}**')
 
 def setup(client):
     client.add_cog(Admin(client))
