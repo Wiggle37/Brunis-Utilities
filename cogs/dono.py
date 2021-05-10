18f74806c30938c3cab580d6b4050e61bb4c28e0
Y33T-Wiggle <misterernie37@gmail.com>
2021-05-10 12:51:16 -0400

    Update dono.py

--- cogs/dono.py
+++ cogs/dono.py
@@ -83,8 +83,6 @@ class Dono(commands.Cog):
         roles_added = []
         for amount, role_id in donors_roles.items():
             if total < amount:
-                continue
-
                 role = discord.utils.find(lambda r: r.id == role_id, ctx.guild.roles)
                 await user.remove_roles(role)
 
