d4068ba5e6e5f05359aefcc9342d369dda3d048c
Y33T-Wiggle <misterernie37@gmail.com>
2021-06-23 21:59:01 -0400

    More image commands

--- cogs/other/images.py
+++ cogs/other/images.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import aiohttp
 import requests
+from requests.models import Response
 
 class Images(commands.Cog, name='images', description='Get some pictures of some animals'):
 
@@ -9,24 +10,73 @@ class Images(commands.Cog, name='images', description='Get some pictures of some
         self.client = client
 
     #Dog
-    @commands.command(name='Dog', description='Get a picture of a dog')
-    async def dog(dog, ctx):
-        async with aiohttp.ClientSession() as session:
-            request = await session.get('https://some-random-api.ml/img/dog')
-            dogjson = await request.json()
+    @commands.command(name='dog', description='Get a random picture of a dog')
+    async def dog(self, ctx):
+        response = requests.get('https://some-random-api.ml/img/dog')
+        data = response.json()
 
-        embed = discord.Embed(title="Doggo!", color=discord.Color.purple())
-        embed.set_image(url=dogjson['link'])
+        embed = discord.Embed(title='Dog Picture 🐶', color=discord.Color.purple())
+        embed.set_image(url=data['link'])
         await ctx.send(embed=embed)
 
     #Cat
     @commands.command(name='Cat', description='Get a random picture of a cat *meow*')
     async def cat(self, ctx):
-        response = requests.get('https://aws.random.cat/meow')
+        response = requests.get('https://some-random-api.ml/img/cat')
         data = response.json()
 
-        embed = discord.Embed(title='Kitty Cat 🐈', color=discord.Colour.purple())
-        embed.set_image(url=data['file'])            
+        embed = discord.Embed(title='Kitty Cat 🐈', color=discord.Color.purple())
+        embed.set_image(url=data['link'])            
+        await ctx.send(embed=embed)
+
+    #Panda
+    @commands.command(name='panda', description='Get a random picture of a panda')
+    async def panda(self, ctx):
+        response = requests.get('https://some-random-api.ml/img/panda')
+        data = response.json()
+
+        embed = discord.Embed(title='Panda 🐼', color=discord.Color.purple())
+        embed.set_image(url=data['link'])
+        await ctx.send(embed=embed)
+
+    #Red Panda
+    @commands.command(name='redpanda', description='Get a random picture of a red panda')
+    async def redpanda(self, ctx):
+        response = requests.get('https://some-random-api.ml/img/red_panda')
+        data = response.json()
+
+        embed = discord.Embed(title='Red Panda 🐼', color=discord.Color.purple())
+        embed.set_image(url=data['link'])
+        await ctx.send(embed=embed)
+
+    #Bird
+    @commands.command(name='bird', description='Get a random picture of a bird')
+    async def bird(self, ctx):
+        response = requests.get('https://some-random-api.ml/img/birb')
+        data = response.json()
+
+        embed = discord.Embed(title='Bird 🐦', color=discord.Color.purple())
+        embed.set_image(url=data['link'])
+        await ctx.send(embed=embed)
+
+    #Fox
+    @commands.command(name='fox', description='Get a random picture of a fox')
+    async def fox(self, ctx):
+        response = requests.get('https://some-random-api.ml/img/fox')
+        data = response.json()
+
+        embed = discord.Embed(title='Fox 🦊', color=discord.Color.purple())
+        embed.set_image(url=data['link'])
+        await ctx.send(embed=embed)
+
+    #Koala
+    @commands.command(name='koala', description='Get a random picture of a koala')
+    async def koala(self, ctx):
+        response = requests.get('https://some-random-api.ml/img/koala')
+        data = response.json()
+
+        embed = discord.Embed(title='Koala 🐨', color=discord.Color.purple())
+        embed.set_image(url=data['link'])
         await ctx.send(embed=embed)
 
 def setup(client):
