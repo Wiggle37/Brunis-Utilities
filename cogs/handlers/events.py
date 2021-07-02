a9923ae63c2be33ac3ddf34ab7a9f13a402e75f5
Y33T-Wiggle <misterernie37@gmail.com>
2021-07-01 22:36:31 -0400

    Update events.py

--- cogs/handlers/events.py
+++ cogs/handlers/events.py
@@ -99,8 +99,7 @@ class Events(commands.Cog, name='Events', command_attrs=dict(hidden=True)):
     #Triggers
     @commands.Cog.listener()
     async def on_message(self, message):
-        user = message.author
-        if str(self.client.user.id) in message.content:
+        if str(self.client.user.id) in message.content and not message.author.bot:
             embed = discord.Embed(title='Hello!', description='My prefix is `b!`\nUse the command `b!help` for help', color=0x00ff00)
             await message.channel.send(embed=embed)
 
