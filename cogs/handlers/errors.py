13a116faca7686523d59230279dbd369e2f048fc
The-Chosent-One <57739806+The-Chosent-One@users.noreply.github.com>
2021-07-13 21:55:00 +0800

    Update errors.py

--- cogs/handlers/errors.py
+++ cogs/handlers/errors.py
@@ -1,41 +1,10 @@
 import discord
 import traceback
 from discord.ext import commands
-import io
 
 class CommandErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    def _get_req(self, command: commands.Command) -> dict:
-        """Gets the required roles/perms required to run a command
-        This is done by intentionally causing an error within the predicate
-        Tracebacks would then show the locals within said predicate
-        
-        Note that this function does not differentiate between roles and perms, 
-        for that, check get_req_roles and get_req_perms, both of which call this function"""
-
-        try:
-            # get the first check
-            check = command.checks[0]
-
-            # We will now intentionally cause an error within the check function
-            # This will error as ctx is passed as 0
-            check(0)
-
-        except Exception as e:
-            # Get the last frame of the generator
-            *frames, last_frame = traceback.walk_tb(e.__traceback__)
-
-            # Getting the first element to get the trace
-            frame = last_frame[0]
-            return frame.f_locals
-    
-    def get_perms_req(self, command: commands.Command) -> dict:
-        return self._get_req(command)["perms"]
-
-    def get_roles_req(self, command: commands.Command) -> set:
-        return self._get_req(command)["items"]
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
@@ -75,31 +44,23 @@ class CommandErrorHandler(commands.Cog):
             if ctx.author.id in self.bot.owner_ids:
                 return await ctx.reinvoke()
 
-            # gets the role ids required to run the command
-            roles_req = self.get_roles_req(ctx.command)
+            # ensures that role_ids will be a list
+            role_ids = [getattr(error, "missing_role")] or getattr(error, "missing_roles")
 
             # gets the names of each role
-            role_names = [discord.utils.get(ctx.guild.roles, id = id).name for id in roles_req]
+            role_name = [discord.utils.get(ctx.guild.roles, id = id).name for id in role_ids]
 
-            return await ctx.send(f"You are missing any of these roles to run a command: `{', '.join(role_names)}`")
+            return await ctx.send(f"You are missing any of these roles to run a command: `{', '.join(role_name)}`")
 
 
         if isinstance(error , commands.MissingPermissions):
-            # gets the perms required to run the command
-            roles_req = self.get_perms_req(ctx.command)
-
             # formats the perms properly
-            perm_names = map(lambda r: r.replace("_", " ").title(), roles_req.keys())
+            perm_names = map(lambda p: p.replace('_', ' ').replace('guild', 'server').title(), error.missing_perms)
 
             return await ctx.send(f"You are these permissions to run this command: {', '.join(perm_names)}")
 
         # else, we send the error to a debug channel
-
-        # we shall print the error straight into stdout and get the tb from there
-        stdout = io.StringIO()
-        print(f"Ignoring exception in command {ctx.command}:", file = stdout)
-        traceback.print_exception(type(error), error, error.__traceback__, file = stdout)
-        tb = stdout.getvalue()
+        tb = "".join(traceback.format_exception(type(error), error, error.__traceback__))
 
         debug = self.bot.get_channel(844759955815006222)
 
