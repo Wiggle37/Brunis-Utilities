1d345b60269dee26f2f79ce56d58d016ef5d2f80
Y33T-Wiggle <misterernie37@gmail.com>
2021-07-15 17:43:26 -0400

    update errors

--- cogs/handlers/errors.py
+++ cogs/handlers/errors.py
@@ -2,28 +2,34 @@ import discord
 import traceback
 from discord.ext import commands
 import traceback
-from discord.ext.commands import errors
-
-from discord.ext.commands.errors import BadArgument, CheckFailure
 
 class CommandErrorHandler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.dank_merchants = self.bot.get_guild(784491141022220309)
-
-    @commands.command()
-    async def fire(self, ctx):
-        await ctx.send('fire pog')
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         error = getattr(error, "original", error)
-        '''
-        ignored = (commands.BadArgument, commands.CheckFailure, commands.CommandOnCooldown)
 
-        if isinstance(error, ignored):
-            return
-        '''
+        # - Command On Cooldown Errors - #
+        if isinstance(error, commands.CommandOnCooldown):
+            return ctx.send('This command is on cooldown, chill out')
+
+        # - Disabled Command - #
+        if isinstance(error, commands.DisabledCommand):
+            return await ctx.send('This command is currently disabled')
+
+        # - Not Found Errors - #
+        if isinstance(error, commands.MessageNotFound):
+            return await ctx.send('The provied message was not found')
+        if isinstance(error, commands.ChannelNotFound):
+            return await ctx.send('The provided channel was not found')
+
+        if isinstance(error, commands.ExtensionNotFound):
+            return await ctx.send('The provided cog was not found')
+
+        if isinstance(error, commands.EmojiNotFound):
+            return await ctx.send('The emoji provided was not found')
 
         if isinstance(error, commands.CommandNotFound):
             return await ctx.send(f"The command `{ctx.message.content}` is not found")
@@ -34,10 +40,7 @@ class CommandErrorHandler(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             return await ctx.send(f"You are missing the `{error.param.name}` argument in the command\n```b!{ctx.command} {ctx.command.signature}```")
 
-        if isinstance(error, commands.BotMissingPermissions):
-            wiggle = self.bot.get_user(824010269071507536)
-            return await wiggle.send(f"Alert! The bot is missing permissions in {ctx.channel.mention} for `{ctx.command}` please get this fixed right away")
-
+        # - Permission Errors - #
         if isinstance(error, commands.NotOwner):
             return await ctx.send("You need to be the owner of the bot to run this command")
 
@@ -46,7 +49,7 @@ class CommandErrorHandler(commands.Cog):
 
         if isinstance(error, commands.MissingAnyRole):
             if ctx.author.id in self.bot.owner_ids:
-                await ctx.send('Your command was reinvoked because you are a bot dev.')
+                await ctx.send('Your command was reinvoked because you are a bot owner.')
                 return await ctx.reinvoke()
             
             role_name = [discord.utils.get(self.dank_merchants.roles, id = id).name for id in error.missing_roles]
@@ -54,10 +57,12 @@ class CommandErrorHandler(commands.Cog):
 
         if isinstance(error, commands.MissingPermissions):
             if ctx.author.id in self.bot.owner_ids:
-                await ctx.send('Your command was reinvoked because you are a bot dev.')
+                await ctx.send('Your command was reinvoked because you are a bot owner.')
                 return await ctx.reinvoke()
 
             return await ctx.send(f'You are missing the following permission to run this command: `{error.missing_perms}`')
+
+
 
         # if none of the above we send to a debug channel
         tb = "".join(traceback.format_exception(type(error), error, error.__traceback__))
@@ -67,7 +72,6 @@ class CommandErrorHandler(commands.Cog):
         # sends each one
         for info in tb_split:
             await debug.send(f"```py\n{info}\n```")    
-    
 
 def setup(bot):
     bot.add_cog(CommandErrorHandler(bot))
