e3a8c8ed2a6e12a057ff58dc94660c014eab768f
Y33T-Wiggle <misterernie37@gmail.com>
2021-07-16 17:50:32 -0400

    making bumps aiosqlite

--- cogs/utils/bump.py
+++ cogs/utils/bump.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 import re
-import sqlite3
+import aiosqlite
 
 from config import *
 
@@ -13,14 +13,14 @@ class BumpTracker(commands.Cog, name='bumps', description='Tracks how much the s
     # Check Bumps
     @commands.command(name='bumps', description='Check the amount of successful and unsuccessful bumps you have in the server', aliases=['b', 'bump'])
     async def bumps(self, ctx, member: discord.Member=None):
-        dbase = sqlite3.connect('bump.db')
-        cursor = dbase.cursor()
+        dbase = await aiosqlite.connect('bump.db')
+        cursor = await dbase.cursor()
         user = (member or ctx.author).id
-        cursor.execute(f"INSERT INTO bumps (user_id) VALUES (?) ON CONFLICT(user_id) DO UPDATE SET user_id = ?;", [user, user])
-        cursor.execute(f"SELECT bump FROM bumps WHERE user_id = '{user}'")
-        bump = cursor.fetchone()[0]
-        cursor.execute(f"SELECT allbumps FROM bumps WHERE user_id = '{user}'")
-        total = cursor.fetchone()[0]
+        await cursor.execute(f"INSERT INTO bumps (user_id) VALUES (?) ON CONFLICT(user_id) DO UPDATE SET user_id = ?;", [user, user])
+        await cursor.execute(f"SELECT bump FROM bumps WHERE user_id = '{user}'")
+        bump = await cursor.fetchone()[0]
+        await cursor.execute(f"SELECT allbumps FROM bumps WHERE user_id = '{user}'")
+        total = await cursor.fetchone()[0]
         user = await ctx.guild.fetch_member(user)
 
         if bump == 0:
@@ -40,7 +40,7 @@ class BumpTracker(commands.Cog, name='bumps', description='Tracks how much the s
             embed.set_footer(text='Big thanks to Firecracker for helping with the bump system')
             await ctx.send(embed=embed)
         
-        dbase.close()
+        await dbase.close()
 
     def valid_message(self, message):
         return message.author.id == 302050872383242240 and message.channel.id == 784994978661138453 and message.embeds != [] 
@@ -58,8 +58,8 @@ class BumpTracker(commands.Cog, name='bumps', description='Tracks how much the s
     # Bump Listener
     @commands.Cog.listener()
     async def on_message(self, message):
-        dbase = sqlite3.connect('bump.db')
-        cursor = dbase.cursor()
+        dbase = await aiosqlite.connect('bump.db')
+        cursor = await dbase.cursor()
 
         bump = 1
 
@@ -75,31 +75,31 @@ class BumpTracker(commands.Cog, name='bumps', description='Tracks how much the s
         user_id = self.get_user_id(embed_desc)
 
         if success:
-            cursor.execute("INSERT INTO bumps (user_id, bump) VALUES (?, ?) ON CONFLICT(user_id) DO UPDATE SET bump = bump + ?;", [user_id, bump, bump])
-            cursor.execute("INSERT INTO bumps (user_id, allbumps) VALUES (?, ?) ON CONFLICT(user_id) DO UPDATE SET allbumps = allbumps + ?;", [user_id, bump, bump])
+            await cursor.execute("INSERT INTO bumps (user_id, bump) VALUES (?, ?) ON CONFLICT(user_id) DO UPDATE SET bump = bump + ?;", [user_id, bump, bump])
+            await cursor.execute("INSERT INTO bumps (user_id, allbumps) VALUES (?, ?) ON CONFLICT(user_id) DO UPDATE SET allbumps = allbumps + ?;", [user_id, bump, bump])
 
-            cursor.execute(f"SELECT bump FROM bumps WHERE user_id = '{user_id}'")
+            await cursor.execute(f"SELECT bump FROM bumps WHERE user_id = '{user_id}'")
             bump = cursor.fetchone()
 
-            cursor.execute(f"SELECT allbumps FROM bumps WHERE user_id = '{user_id}'")
-            total = cursor.fetchone()
+            await cursor.execute(f"SELECT allbumps FROM bumps WHERE user_id = '{user_id}'")
+            total = await cursor.fetchone()
 
             precentage = int(round(bump[0] / total[0], 2) * 100)
 
-            cursor.execute(f"UPDATE bumps SET precentage = '{precentage}' WHERE user_id == {user_id}")
+            await cursor.execute(f"UPDATE bumps SET precentage = '{precentage}' WHERE user_id == {user_id}")
 
         else:
-            cursor.execute("INSERT INTO bumps (user_id, allbumps) VALUES (?, ?) ON CONFLICT(user_id) DO UPDATE SET allbumps = allbumps + ?;", [user_id, bump, bump])
+            await cursor.execute("INSERT INTO bumps (user_id, allbumps) VALUES (?, ?) ON CONFLICT(user_id) DO UPDATE SET allbumps = allbumps + ?;", [user_id, bump, bump])
             
-            cursor.execute(f"SELECT bump FROM bumps WHERE user_id = '{user_id}'")
+            await cursor.execute(f"SELECT bump FROM bumps WHERE user_id = '{user_id}'")
             bump = cursor.fetchone()
 
-            cursor.execute(f"SELECT allbumps FROM bumps WHERE user_id = '{user_id}'")
-            total = cursor.fetchone()
+            await cursor.execute(f"SELECT allbumps FROM bumps WHERE user_id = '{user_id}'")
+            total = await cursor.fetchone()
 
             precentage = int(round(bump[0] / total[0], 2) * 100)
 
-            cursor.execute(f"UPDATE bumps SET precentage = '{precentage}' WHERE user_id == {user_id}")
+            await cursor.execute(f"UPDATE bumps SET precentage = '{precentage}' WHERE user_id == {user_id}")
 
         cursor.execute("SELECT user_id, MAX(bump) FROM bumps;")
         top = cursor.fetchone()
@@ -114,8 +114,8 @@ class BumpTracker(commands.Cog, name='bumps', description='Tracks how much the s
 
         await user.add_roles(role)
 
-        dbase.commit()
-        dbase.close()
+        await dbase.commit()
+        await dbase.close()
 
 def setup(bot):
     bot.add_cog(BumpTracker(bot))
