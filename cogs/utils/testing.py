a7c56acc9516429f3fa810cb0bb17ad3939435a1
Y33T-Wiggle <misterernie37@gmail.com>
2021-07-23 18:12:18 -0400

    wow am i so dumb

--- cogs/utils/testing.py
+++ cogs/utils/testing.py
@@ -8,5 +8,10 @@ class Testing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def test(self, ctx):
+        from config import CONFIG
+        return await ctx.send(CONFIG["settings"])
+
 def setup(bot):
     bot.add_cog(Testing(bot))
