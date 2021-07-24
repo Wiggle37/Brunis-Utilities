66d78c71799bef5df8d9c244126a6b79a82257b6
Y33T-Wiggle <misterernie37@gmail.com>
2021-07-24 12:38:02 -0400

    e

--- buttons.py
+++ buttons.py
@@ -17,3 +17,18 @@ class Confirm(discord.ui.View):
     async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
         self.value = False
         self.stop()
+
+class ChosePremium(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    @discord.ui.button(label='Premium', style=discord.ButtonStyle.green)
+    async def premium(self, button: discord.ui.Button, interaction: discord.Integration):
+        self.value = True
+        self.stop()
+
+    @discord.ui.button(label='Not Premium', style=discord.ButtonStyle.red)
+    async def nopremium(self, button: discord.ui.Button, interaction: discord.Integration):
+        self.value = False
+        self.stop()
--- cogs/utils/staff.py
+++ cogs/utils/staff.py
@@ -4,7 +4,10 @@ from discord.ext import commands
 import aiosqlite
 import sqlite3
 
+from discord.ui import view
+
 from config import *
+from buttons import *
 
 class Staff(commands.Cog, name = "Staff", description = "Commands only staff can use"):
     def __init__(self, bot):
@@ -59,22 +62,30 @@ class Staff(commands.Cog, name = "Staff", description = "Commands only staff can
     @commands.command(name='add_auto_response', description='Add an auto response', aliases=['ara'])
     @commands.has_any_role(784492058756251669, 788738308879941633, 784528018939969577)
     async def ara(self, ctx, trigger, *, response):
-        dbsae = await aiosqlite.connect('autoresponse.db')
-        cursor = await dbsae.cursor()
+        view = ChosePremium()
+        async with aiosqlite.connect('autoresponse.db') as dbase:
+            cursor = await dbase.execute(f"SELECT trigger FROM text WHERE trigger == ?", [trigger])
+            exist = await cursor.fetchone()
+            if exist is None:
+                await dbase.execute(f"INSERT INTO text (trigger) VALUES (?) ON CONFLICT(trigger) DO UPDATE SET trigger = ?", [trigger, trigger])
+                await dbase.execute(f"UPDATE text SET response = ? WHERE trigger == '{trigger}'", [response])
+                await dbase.commit()
 
-        await cursor.execute(f"SELECT trigger FROM text WHERE trigger == ?", [trigger])
-        exist = await cursor.fetchone()
-        if exist is None:
-            await cursor.execute(f"INSERT INTO text (trigger) VALUES (?) ON CONFLICT(trigger) DO UPDATE SET trigger = ?", [trigger, trigger])
-            await cursor.execute(f"UPDATE text SET response = ? WHERE trigger == '{trigger}'", [response])
+                await ctx.send(f'A new Trigger has been added with the following information:\nTrigger: {trigger}\nResponse: {response}', view=view)
+                await view.wait()
+                if view.value is None:
+                    return await ctx.send('Timed out, this trigger will be useable by everyone in the server')
+                
+                elif view.value:
+                    await dbase.execute(f"UPDATE text SET premium = ? WHERE trigger = '{trigger}'", [True])
+                    await dbase.commit()
+                    return await ctx.send('Ok, this trigger will only be usable by premium members')
+                
+                elif not view.value:
+                    return await ctx.send('Ok, this trigger will be useable by all members of the server')
 
-            await ctx.send(f'A new Trigger has been added with the following information:\nTrigger: {trigger}\nResponse: {response}')
-
-        else:
-            await ctx.send('This trigger already exists')
-
-        await dbsae.commit()
-        await dbsae.close()
+            elif exist is not None:
+                await ctx.send('This trigger already exists')
 
     # Remove Auto Response
     @commands.command(name='remove_auto_response', description='Remove an auto response', aliases=['arr'])
@@ -103,22 +114,32 @@ class Staff(commands.Cog, name = "Staff", description = "Commands only staff can
     @commands.command(name='add_auto_reaction', description='Add an emoji reaction', aliases=['aea'])
     @commands.has_any_role(784492058756251669, 788738308879941633, 784528018939969577)
     async def aea(self, ctx, trigger, emoji):
-        dbase = await aiosqlite.connect('autoresponse.db')
-        cursor = await dbase.cursor()
+        view = ChosePremium()
+        async with aiosqlite.connect('autoresponse.db') as dbase:
+            cursor = await dbase.execute(f"SELECT trigger FROM emoji WHERE trigger == ?", [trigger])
+            exist = await cursor.fetchone()
+            if exist is None:
+                await dbase.execute(f"INSERT INTO emoji (trigger) VALUES (?) ON CONFLICT(trigger) DO UPDATE SET trigger = ?", [trigger, trigger])
+                await dbase.execute(f"UPDATE emoji SET emoji = ? WHERE trigger = '{trigger}'", [emoji])
+                await dbase.commit()
 
-        await cursor.execute(f"SELECT trigger FROM emoji WHERE trigger == ?", [trigger])
-        exist = await cursor.fetchone()
-        if exist is None:
-            await cursor.execute(f"INSERT INTO emoji (trigger) VALUES (?) ON CONFLICT(trigger) DO UPDATE SET trigger = ?", [trigger, trigger])
-            await cursor.execute(f"UPDATE emoji SET emoji = ? WHERE trigger == '{trigger}'", [emoji])
+                await ctx.send('Emoji response added!', view=view)
 
-            await ctx.send('Emoji response added!')
+                await view.wait()
+                if view.value is None:
+                    return await ctx.send('Response timed out, this trigger will be useable by all users')
 
-        elif exist != None:
-            return await ctx.send('This trigger already exists!')
+                elif view.value:
+                    dbase.execute(f"UPDATE emoji SET premium = ? WHERE trigger = '{trigger}'", [True])
+                    await dbase.commit()
+                    return await ctx.send('Ok, this trigger will only be useable by premium users')
 
-        await dbase.commit()
-        await dbase.close()
+                elif not view.value:
+                    return await ctx.send('This trigger will be useable by all members of the server')
+
+            elif exist is not None:
+                return await ctx.send('This trigger already exists!')
+
 
     # Emoji Remove Response
     @commands.command(name='remove_auto_reaction', description='Remove an emoji reaction', aliases=['aer'])
@@ -155,7 +176,7 @@ class Staff(commands.Cog, name = "Staff", description = "Commands only staff can
             return await ctx.send('That is not a valid option plese user either: `True` or `False`')
 
         elif mode or not mode:
-            CONFIG["config"]["settings"]["heistmode"] = mode
+            CONFIG["settings"]["heistmode"] = mode
             with open('config.json', 'w') as file:
                 json.dump(CONFIG, file, indent=4)
                 f.close()
