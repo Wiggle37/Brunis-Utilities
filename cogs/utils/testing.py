4b98af754bacfa595d7588ff9409078a528c1733
Y33T-Wiggle <misterernie37@gmail.com>
2021-07-27 09:56:43 -0400

    update testing

--- cogs/utils/testing.py
+++ cogs/utils/testing.py
@@ -14,19 +14,5 @@ class Testing(commands.Cog):
     async def test(self, ctx):
         await ctx.send(f'<t:{int((ctx.author.created_at.timestamp() - 1814400) + time.time())}:R>')
 
-    @commands.command()
-    @commands.is_owner()
-    async def friskygae(self, ctx):
-        channel = self.bot.get_channel(784493280498155542)
-        message = await channel.fetch_message(869087109057761301)
-        users = []
-        for reaction in message.reactions:
-            async for user in reaction.users():
-                users.append(f'{user.mention}')
-        
-        split = [users[i:i+1755] for i in range(0, len(users), 1755)]
-        for info in split:
-            await ctx.send(f"{info}")
-
 def setup(bot):
     bot.add_cog(Testing(bot))
